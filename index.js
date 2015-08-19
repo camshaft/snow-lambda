54fb37df23d1d798cd845dd643fdacb794a5c690
camshaft <bytheway.cameron@gmail.com>
2015-08-19 15:16:26 -0600

    implement lambda function

--- index.js
+++ index.js
@@ -2,6 +2,34 @@
  * Module dependencies
  */
 
-exports.index = function() {
+var pipeline = require('./lib/pipelines/default');
+var AWS = require('aws-sdk');
+var zlib = require('zlib');
+var s3 = new AWS.S3();
 
+exports.handler = function(event, context) {
+  var bucket = event.Records[0].s3.bucket.name;
+  var key = event.Records[0].s3.object.key;
+  key = decodeURIComponent(key.replace(/\+/g, " "));
+  s3.getObject({
+    Bucket: bucket,
+    Key: key
+  }, function(err, response) {
+    if (err) return context.fail(err);
+    zlib.gunzip(response.Body, function(err, logs) {
+      if (err) return context.fail(err);
+      var rows = pipeline(logs.toString());
+      zlib.gzip(rows, function(err, compressed) {
+        if (err) return context.fail(err);
+        s3.putObject({
+          Bucket: bucket,
+          Key: key.replace('logs/', 'enriched/'),
+          Body: compressed,
+          ContentType: 'application/x-gzip'
+        }, function(err) {
+          context.done(err);
+        });
+      });
+    });
+  });
 };
--- test/example.js
+++ test/example.js
@@ -1,21 +1,6 @@
-var cfp = require('../lib/parsers/cloudfront');
-var cft = require('../lib/translators/cloudfront');
-var snowplow = require('../lib/parsers/snowplow');
-var enrichments = [
-  require('../lib/enrichers/user-agent'),
-  require('../lib/enrichers/referer'),
-  require('../lib/enrichers/ip-address'),
-];
+var pipeline = require('../lib/pipelines/default');
 
 var read = require('fs').readFileSync;
 var text = read(__dirname + '/parsers/sample.log', 'utf8');
 
-var lines = cfp(text).map(function(line) {
-  if (line.error) return line;
-  line = cft(line, snowplow);
-  return enrichments.reduce(function(acc, fn) {
-    return fn(acc);
-  }, line);
-});
-
-console.log(JSON.stringify(lines, null, '  '));
+process.stdout.write(pipeline(text));
