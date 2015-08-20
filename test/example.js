cba25329b16f5a3b28987fa27017e9ae76f7e168
camshaft <bytheway.cameron@gmail.com>
2015-08-19 18:13:31 -0600

    implement url/referrer parsing

--- test/example.js
+++ test/example.js
@@ -3,4 +3,6 @@ var pipeline = require('../lib/pipelines/default');
 var read = require('fs').readFileSync;
 var text = read(__dirname + '/parsers/sample.log', 'utf8');
 
-process.stdout.write(pipeline(text));
+var out = pipeline(text);
+
+// process.stdout.write(out);
